8029c7cb083dd1b933150bacb0561775b8172474
Kraken15th <ahmedkarrar79@gmail.com>
2024-09-16 00:05:11 +0300

    Add files via upload

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# future_age_calculator.py
+current_year = 2023  # Assuming the current year
+# Prompt the user for their age
+current_age = int(input("How old are you? "))
+# Calculate age in 2050
+age_in_2050 = current_age + (2050 - current_year)
+# Print the result
+print(f"In 2050, you will be {age_in_2050} years old.")
