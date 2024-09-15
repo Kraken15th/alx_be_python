5b477a9ae69e3c0620dda1ba253d9e54547a31b2
Kraken15th <ahmedkarrar79@gmail.com>
2024-09-15 23:17:11 +0300

    Add files via upload

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,13 @@
+# basic_operations.py
+number1 = 10
+number2 = 5
+# Sum
+sum_result = number1 + number2
+print(f"Sum of {number1} and {number2} is {sum_result}")
+# Difference
+difference_result = number1 - number2
+print(f"Difference of {number1} and {number2} is {difference_result}")
+# Product
+product_result = number1 * number2
+print(f"Product of {number1} and {number2} is {product_result}")
 
