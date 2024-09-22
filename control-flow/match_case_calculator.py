394de91073427de166a5250b89066703b42b13a6
Kraken15th <ahmedkarrar79@gmail.com>
2024-09-23 00:38:37 +0300

    Add files via upload

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,14 @@
+# Get user input for numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+# Get user input for operation
+operation = input("Choose the operation (+, -, *, /): ")
+# Perform calculation using Match Case
+# case "+":
+print(num1 + num2)
+# case "-":
+print(num1 - num2)
+# case "*":
+print(num1 * num2)
+# case "/":
+print(num1 / num2)
