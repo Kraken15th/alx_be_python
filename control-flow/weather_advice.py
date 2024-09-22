c199494e700d4e88a8982fdd3997c2209fdae6b8
Kraken15th <ahmedkarrar79@gmail.com>
2024-09-22 23:57:49 +0300

    Add files via upload

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,9 @@
-#def main : (weather):
-#weather = input("What's the weather like today? (sunny/rainy/cold): ")
-#if weather.lower() == "sunny":
-#print("Wear a t-shirt and sunglasses.")
-#if weather.lower() == "rainy":
-#print("Don't forget your umbrella and a raincoat.")
-#if weather.lower() == "cold":
-#else:
-#print("Sorry, I don't have recommendations for this weather.")
-#if __name__ == "__main__":
+weather = input("What's the weather like today? (sunny/rainy/cold):")
+if weather == "sunny":
+ print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+  print ("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+  print ("Make sure to wear a warm coat and a scarf.")
+else:
+  print ("Sorry, I don't have recommendations for this weather.")
