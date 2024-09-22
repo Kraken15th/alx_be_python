2c1e77eab66d3ae8aeb782f5649af5e9375ce73e
Kraken15th <ahmedkarrar79@gmail.com>
2024-09-22 16:12:06 +0300

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,10 @@
-def main():
-weather = input("What's the weather like today? (sunny/rainy/cold): ")
-if weather.lower() == "sunny":
-print("Wear a t-shirt and sunglasses.")
-elif weather.lower() == "rainy":
-print("Don't forget your umbrella and a raincoat.")
-elif weather.lower() == "cold":
-else:
-print("Sorry, I don't have recommendations for this weather.")
-if __name__ == "__main__":
-main()
+#def main : (weather):
+#weather = input("What's the weather like today? (sunny/rainy/cold): ")
+#if weather.lower() == "sunny":
+#print("Wear a t-shirt and sunglasses.")
+#if weather.lower() == "rainy":
+#print("Don't forget your umbrella and a raincoat.")
+#if weather.lower() == "cold":
+#else:
+#print("Sorry, I don't have recommendations for this weather.")
+#if __name__ == "__main__":
