7eaf590d86ed72dddfa6cea3d817518fb17b9869
Kraken15th <ahmedkarrar79@gmail.com>
2024-09-23 00:42:53 +0300

    Add files via upload

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -12,3 +12,10 @@ print(num1 - num2)
 print(num1 * num2)
 # case "/":
 print(num1 / num2)
+if num2 == 0:
+ print ("Cannot divide by zero.")
+else:
+ print(num1 / num2)
+# case _:
+print ("Invalid operation.")
+# Display the result
