e7fd663fd66534b24a8fd63bd3de6ab77588c692
Kraken15th <ahmedkarrar79@gmail.com>
2024-10-03 21:33:01 +0300

    Add files via upload

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,3 +1,4 @@
+from random import choice
 def display_menu():
     print("Shopping List Manager")
     print("1. Add Item")
@@ -9,25 +10,25 @@ def display_menu():
      while True:
         display_menu()
         choice = input("Enter your choice: ")
-         choice == float
-if input == "1":
+        choice == float
+if choice == "1":
    # Prompt for and add an item
    item = input("Enter item name: ")
  shopping_list.append(item) # type: ignore
 print(f"{item} added to the list.")
-if input == '2':
+if choice == '2':
    # Prompt for and remove an item
    item_to_remove = input("Enter item name to remove: ")
    if item_to_remove in shopping_list: # type: ignore
       shopping_list.remove(item_to_remove) # type: ignore
       print(f"{item_to_remove} removed from the list.")
 print(f"{item_to_remove} not found in the list.")
-if input == '3':
+if choice == '3':
 # Display the shopping list
  if shopping_list: # type: ignore
    print("Shopping List:")
  for item in shopping_list: # type: ignore
    print(f"- {item}")
-elif input == '4':
+elif choice == '4':
  print("Goodbye!")
 print("Invalid choice. Please try again.")
